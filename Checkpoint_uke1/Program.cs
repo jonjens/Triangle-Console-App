7f17857bf907f30f6ef2e6469d3144559909e736
jonjens <jonassenjens@gmail.com>
2020-11-05 20:11:25 +0100

    Add project files.

---
+++ Checkpoint_uke1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Checkpoint_uke1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Build.Start();
+
+            Build.StartTwo();
+
+
+
+        }
+
+    }
+
+    public class Build
+    {
+            
+        public static void Start()
+        {
+            Triangle t = new Triangle();
+            t.BuildTriangle();
+        }
+
+        public static void StartTwo()
+        {
+            TriangleA tA = new TriangleA();
+
+            tA.BuildTriangle();
+        }
+
+
+    }
+    
+}
---
+++ Checkpoint_uke1/Triangle.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Checkpoint_uke1
+{
+
+    public class Triangle
+    {
+
+
+        public void BuildTriangle()
+        {
+
+            Console.WriteLine("Skriv inn siffer som angir lengde på triangelen adskilt med '-'");
+
+            string[] Values = Console.ReadLine().Split('-');
+            string Star = "";
+            foreach (var item in Values)
+            {
+
+                int Input = Convert.ToInt32(item);
+
+                for (int i = 0; i < Input; i++)
+                {
+                    Star += "*";
+                    Console.WriteLine(Star);
+
+                }
+
+                Star = "";
+
+                Console.WriteLine();
+
+            }
+
+        }
+
+    }
+
+    public class TriangleA
+    {
+        public void DrawLine(int length)
+        {
+            for (int j = 1; j <= length; j++)
+            {
+                Console.Write('*');
+            }
+            Console.Write('\n');
+        }
+        public void BuildTriangle()
+        {
+
+            Console.WriteLine("Skriv inn bokstav og siffer som angir lengde på triangelen adskilt med '-' hvor A er voksende triangel og B er krympende");
+
+            string[] Values = Console.ReadLine().Split('-');
+
+
+
+            string Star = "";
+            foreach (var item in Values)
+            {
+
+                if (item.StartsWith('A'))
+                {
+                    int Input = Convert.ToInt32(item.Substring(1));
+
+                    for (int i = 0; i < Input; i++)
+                    {
+                        DrawLine(i);
+                    }
+
+                }
+                else if(item.StartsWith('B'))
+                {
+                    int Input = Convert.ToInt32(item.Substring(1));
+                    
+                    for (int i = Input; i >= 1; i--)
+                    {
+                        DrawLine(i);
+
+                    }
+
+
+                }
+
+
+                Console.WriteLine();
+
+            }
+
+        }
+
+
+
+
+    }
+
+}
